8331ec2230c7268e62dd3af3ead55a8f1754f4b3
Administrator <416326391@qq.com>
2019-04-26 15:27:08 +0800

    aletr

---
+++ day01/basic.py
@@ -0,0 +1,48 @@
+
+
+# comment
+
+'''
+comments...
+'''
+
+s = """
+comments...
+"""
+
+print('test')
+
+print(s)
+
+x = 1
+
+print(0xff)
+
+print(1.234e4)
+
+print('abc')
+
+print('Tom\'s name')
+print(r'Tom\'s name')  # raw
+
+print('line\nanother line...')
+
+# <pre>
+
+print("""
+    白日依山尽，
+    黄河入海流。
+""")
+
+print('a', 'bc', 'def')
+print('a' + 'bc' + 'def')
+
+# Scanner
+"""
+Scanner scanner = new Scanner(System.in);
+int i = scanner.nextInt();
+System.out.println(i);
+// 123
+"""
+
+print(input())
