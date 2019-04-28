c19b090576904dfa5036c89bed353be336608450
Administrator <416326391@qq.com>
2019-04-28 09:29:44 +0800

    aletr

--- day02/if_test.py
+++ day02/if_test.py
@@ -22,14 +22,15 @@ print('x')
 # python 不换行输出 x ？
 
 print('x', end='\n')
-print('x', end=' ')  # 不换行输出
-print('x', end=' ')
+print('a', end=' ')  # 不换行输出
+print('b', end=' ')
 
-print('x', end='')
-print('x', end='')
+print('c', end='')
+print('d', end='')
 
-print('k', end='\n' )  # \n : 代表换行
+print('e', end='\n' )  # \n : 代表换行
 
 print(' ', end='')  # 1  ： 开头空一格
-print('x', end='')  # 2
-print('x')  # 3
+print('f', end='')  # 2
+print('g')  # 3
+
