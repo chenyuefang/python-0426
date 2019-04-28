10e9da5d1f2db150fd4446bf58f9b552ed81e8f9
Administrator <416326391@qq.com>
2019-04-28 09:05:12 +0800

    aletr

---
+++ day02/if_test.py
@@ -0,0 +1,22 @@
+score = 70
+
+if score > 60:
+    print('pass')
+
+if score > 60:
+    print('pass')
+else:
+    print('failed')
+
+if score >= 90:
+    print('A')
+elif score >= 80:
+    print('B')
+else:
+    print('C')
+
+
+print('x')
+print('x')
+
+# python 不换行输出 x ？
