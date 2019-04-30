1de06fcd82e812ed184d9cecac2888c2f3dbf5e8
Administrator <416326391@qq.com>
2019-04-30 10:22:44 +0800

    aletr

---
+++ day04/read_test.py
@@ -0,0 +1,45 @@
+import os
+
+from urllib.request import urlopen
+
+# f = open('C:/python-0426.yaml', encoding='utf-8')
+# print(f.read())
+# f.close()
+
+with open('C:/python-0426.yaml', encoding='utf-8') as f:
+    # print(f.read(3))
+    pass
+
+with open('C:/python-0426.yaml', encoding='utf-8') as f:
+    pass
+    # line = f.readline()
+    # while line:
+    #     print(line.strip())
+    #     line = f.readline()
+
+s = ' Hello, World! '
+# print(s.strip())
+# print(s.lstrip())  # l:left
+# print(s.rstrip())  # r:right
+
+with open('C:/python-0426.yaml', encoding='utf-8') as f:
+    for x in f:
+        # print(x.strip())
+        pass
+
+with open('C:/python-0426.yaml', encoding='utf-8') as f:
+    for line in f.readlines():
+        # print(line.strip())
+        pass
+
+with open('1.jpg', 'rb') as f:
+    # print(f.read())
+    pass
+
+# print(os.getcwd())  # current working directory
+
+with urlopen('https://book.douban.com/tag/%E5%B0%8F%E8%AF%B4') as f:
+    for line in f.readlines():
+        line = line.decode('utf-8')
+        if 'img class=""' in line:
+            print(line.strip()[len('<img class="" src="'):len(line.strip())-1])
