358f51b71b6a6494d5c8ea94a75c3646ec4b1bce
Administrator <416326391@qq.com>
2019-04-30 08:36:52 +0800

    aletr

--- day03/tuple_test.py
+++ day03/tuple_test.py
@@ -4,33 +4,33 @@ print(names)
 
 numbers = (1,)
 
-print(numbers)  # (1)
+print(numbers)  # (1)  声明
 
-print(names[-1])
+print(names[-1])  # 访问元素
 
 # print(numbers[1])
 
-print(2 not in numbers)
+print(2 not in numbers)  # 判断元素是否存在
 
-print(len(names))
+print(len(names))  # 元祖长度
 
-names = tuple(('test', 'test'))
+names = tuple(('test', 'test'))   # 元祖构造器
 
 print(names)
 
-print(names.count('test'))
+print(names.count('test'))  # 元素统计
 
-print(names.index('test'))
+print(names.index('test'))  # 返回元素索引
 
 superstars = ['Tom', 'Jerry']
 names = (superstars, 'Spike')
 
 print(names)
 
-names[0].append('Tester')
+names[0].append('Tester')  #  不可变的理解
 
 print(names)
 
-for name in names:
+for name in names:   # 迭代
     print(name)
 
