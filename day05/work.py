0617e823731279ed0f19d3a96d9325bf778830f8
Administrator <416326391@qq.com>
2019-05-05 11:51:01 +0800

    aletr

--- day05/work.py
+++ day05/work.py
@@ -1,13 +1,18 @@
 import string
 
+arr=input()
 
-def fn_str(str):
-    m,n = 0
-    if str in string.ascii_letters:
-        return 'ascii_letters'  # ASCII 字母
-        # if str in string.ascii_uppercase:
-        #     n += n
-        # else: str in string.ascii_lowercase:
-        #         m += m
-    else:
-        return '-1'
+
+def fn_str(arr):
+    while (arr in string.ascii_letters):
+        if arr in string.ascii_uppercase:
+            n: int = 0
+            print('ascii_uppercase')
+            n += n
+            print(n)
+        elif arr in string.ascii_lowercase:
+            m: int = 0
+            print('ascii_lowercase')
+            m += m
+            print(m)
+    return -1
