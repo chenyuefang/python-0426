4702014d87326cde006dfa1d78cbddd6e9d1f078
Administrator <416326391@qq.com>
2019-05-05 11:18:18 +0800

    aletr

--- day05/params_test.py
+++ day05/params_test.py
@@ -31,12 +31,12 @@ def fn_default(x, y=1, z=2):
     return x + y - z
 
 
-print(fn_default(0, 1))
-print(fn_default(1, z=1))
+print(fn_default(0, 1))  # 1 为 y=1  调用，默认参数可按照顺序，不必写参数名
+print(fn_default(1, z=1))  # 默认参数不按照顺序调用，需要写明参数名
 
 
 # 可变参数
-
+# 位置参数在前，默认参数在后； 把变化小的参数作为默认参数； 默认参数必须只想不变的对象
 # 可变类型作为默认参数 array=【】  第二个print(fn_append())  得到 ['END' , 'END']
 def fn_append(array=None):
     if array is None:
@@ -58,14 +58,16 @@ void method(int... x)
 
 # * + 参数名 ；参数个数可变；可变参数被组装为tuple
 def fn_sum(*numbers):
+    print(numbers)
     s = 0
     for n in numbers:
         s += n
-        return s
+    return s
 
-    print(fn_sum(1, 2, 3, 4, 5))
-    # print(fn_sum())
 
-    num = [1, 2, 3, 4, 5]
+print(fn_sum(1, 2, 3, 4, 5))
+# print(fn_sum())
 
-    print(fn_sum(num[0], num[1], num[2], num[3], num[4]))
+num = [1, 2, 3, 4, 5]
+
+print(fn_sum(num[0], num[1], num[2], num[3], num[4]))
---
+++ day05/work.py
@@ -0,0 +1,13 @@
+import string
+
+
+def fn_str(str):
+    m,n = 0
+    if str in string.ascii_letters:
+        return 'ascii_letters'  # ASCII 字母
+        if str in string.ascii_uppercase:
+            n += n
+        else: str in string.ascii_lowercase:
+                m += m
+    else:
+        return '-1'
