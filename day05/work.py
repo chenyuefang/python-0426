e7632433cbaef356c06db740e5c6a5adc5481705
Administrator <416326391@qq.com>
2019-05-05 16:39:08 +0800

    aletr

--- day05/work.py
+++ day05/work.py
@@ -1,18 +1,43 @@
+# 1
 import string
 
-arr=input()
+a = str(input())
 
 
-def fn_str(arr):
-    while (arr in string.ascii_letters):
-        if arr in string.ascii_uppercase:
+def fn_str(a):
+    while a in string.ascii_letters:
+        if a in string.ascii_uppercase:
             n: int = 0
             print('ascii_uppercase')
             n += n
             print(n)
-        elif arr in string.ascii_lowercase:
+        elif a in string.ascii_lowercase:
             m: int = 0
             print('ascii_lowercase')
             m += m
             print(m)
     return -1
+
+
+# 2
+
+li = [1, 2, 3, 4, 5, 1, 2, 3]
+
+li = list(set(li))
+
+print(li)
+
+
+# 3.
+
+
+def fn_fibonacci(n):
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fn_fibonacci(n - 1) + fn_fibonacci(n - 2)
+
+
+print(fn_fibonacci(10))
