1d74634505d962b6f45d7191a8825976aa4c60b0
Administrator <416326391@qq.com>
2019-05-05 11:40:50 +0800

    aletr

--- day05/params_test.py
+++ day05/params_test.py
@@ -71,3 +71,44 @@ print(fn_sum(1, 2, 3, 4, 5))
 num = [1, 2, 3, 4, 5]
 
 print(fn_sum(num[0], num[1], num[2], num[3], num[4]))
+
+
+# 关键字参数
+def fn_keywords(email, password, **kw):
+    print(email, password)
+    if 'gende' in kw:
+        # todo
+        pass
+    if 'married' in kw:
+        # todo
+        pass
+
+
+fn_keywords('tom@tom.com', 123, gende='male', married=False)
+
+
+# 命名关键字参数【 *， + 参数名】
+def fn_named_keywords(email, password, *, age, married=False, **kw):
+    print(email, password, age, married, kw)
+
+
+fn_named_keywords('jerry@tom.com', 'abc', age=18)  # married 默认为 False
+fn_named_keywords('jerry@tom.com', 'abc', age=18, married=True)
+fn_named_keywords('jerry@tom.com', 'abc', age=18, married=True, gende='male')
+
+
+def fn_named_keywords(email, password, *args, age, married=False, **kw):
+    print(email, password, args, age, married, kw)
+
+
+fn_named_keywords('email...', 'password...', 1, 2, 3, 'abc', age=19, gende='female')
+
+
+# 参数顺序
+
+
+def fn_test(a, b=1, *c, d, **e):  # d:命名关键字参数
+    print(a, b, c, d, e)
+
+
+fn_test(1, 2, 3, 4, 5, d=6)  # 1 2[修改b的默认值] (3,4,5)[*c的值] 6[必须写出参数名] {}
--- day05/work.py
+++ day05/work.py
@@ -5,9 +5,9 @@ def fn_str(str):
     m,n = 0
     if str in string.ascii_letters:
         return 'ascii_letters'  # ASCII 字母
-        if str in string.ascii_uppercase:
-            n += n
-        else: str in string.ascii_lowercase:
-                m += m
+        # if str in string.ascii_uppercase:
+        #     n += n
+        # else: str in string.ascii_lowercase:
+        #         m += m
     else:
         return '-1'
