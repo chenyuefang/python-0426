a3ea26f7c90ed6f28b6f3294112648eae58444f5
Administrator <416326391@qq.com>
2019-05-06 09:27:19 +0800

    aletr

--- day06/class_test.py
+++ day06/class_test.py
@@ -14,14 +14,46 @@ class ClassName(object):
 
 class Human(object):
 
-    def __init__(self, name, age):  # 强制绑定
-        self.name = name   # self  类似于Java 语言中的 this
-        self.age = age
+    def __init__(self, name, age, gender):  # 强制绑定
+        self.name = name  # self  类似于Java 语言中的 this
+        self.__age = age
+        self._gender = gender
 
 
-human = Human('Jerry', 18)  # 创建实例
-print(type(human))
+def fun_test(self):
+    pass
 
-# 自有绑定属性
-human.name = 'Tom'  # 直接加 . 进行绑定赋值
-print(human.name)
+
+def get_name(self):
+    return self.name
+
+
+def get_age(self):
+    return self.__age
+
+
+def set_age(self, age):
+    if age > 0:
+        self.__age = age
+    else:
+        raise ValueError('Error: age < 0')
+
+
+# human = Human('Jerry', 18)  # 创建实例
+# print(type(human))
+#
+# # 自有绑定属性
+# human.name = 'Tom'  # 直接加 . 进行绑定赋值
+# print(human.name)
+
+tom = Human('Tom', 18, 'male')
+
+print(tom.name)
+# print(tom.__age)  # 私有属性
+# print(tom._gender)  # 视为私有属性
+
+print(tom.get_age())
+
+tom.set_age(-1)
+
+print(tom.get_age())
