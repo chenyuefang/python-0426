5ec2392937840a9501b7a958768cb4fc9a415cda
Administrator <416326391@qq.com>
2019-05-06 11:22:13 +0800

    aletr

--- day06/inherit_test.py
+++ day06/inherit_test.py
@@ -1,3 +1,8 @@
+#   继承
+#  子类继承父类的属性和方法
+# 子类可以有新的属性和方法
+
+
 class Human(object):
     def __init__(self, name):
         self.name = name
@@ -18,7 +23,8 @@ zhangsan = Chinese('Zhang san')
 zhangsan.study()
 
 print(zhangsan.name)
-
+# 多态的实现   子类覆盖 override 或重写 overwrite 父类的方法
+# isinstance
 print(isinstance(zhangsan, Chinese))
 print(isinstance(zhangsan, Human))
 
@@ -28,6 +34,8 @@ def fn_study(human):
 
 
 fn_study(zhangsan)
+
+# 鸭子类型  Duck typing
 
 
 class Duck(object):
@@ -40,9 +48,11 @@ yellow = Duck()
 
 fn_study(yellow)
 
+# type 判断对象类型
 print(type(abs))
 print(type(fn_study))
 
+# dir  获取对象的属性和方法
 print(dir(zhangsan))
 
 print(zhangsan.__class__)
