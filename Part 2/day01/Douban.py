b99bd40e85e4052c47d86baca5b91febc36636ad
Administrator <416326391@qq.com>
2019-05-10 11:50:17 +0800

    aletr

--- Part 2/day01/Douban.py
+++ Part 2/day01/Douban.py
@@ -7,8 +7,23 @@ html = requests.get("https://movie.douban.com/top250")
 # 解析文档
 # 获取豆瓣top250中页面的标题
 selector = etree.HTML(html.text)
-title = selector.xpath("/html/head/title/text()")  #  text 获取文字内容
-name = selector.xpath("//div[@class='hd']/a/span[1]/text()")
-print(name)
 
-# @属性名  @href
+all_moives = []
+
+movies_list = selector.xpath("//ol[@class='grid_view']/li")  # @属性名  @href
+for movie_selector in movies_list:
+    name = movie_selector.xpath(".//div[@class='hd']/a/span[1]/text()")[0]  # 名称
+    star = movie_selector.xpath(".//div[@class='star']/span[@class='rating_num']/text()")[0]  # 评分
+    nums = movie_selector.xpath(".//div[@class='star']/span[last()]/text()")[0]  # 人数
+    nums = nums.split("人评价")[0]
+    url = movie_selector.xpath(".//div[@class='hd']/a/@href")[0]
+    one_movie = [name, star, nums, url]
+
+    all_moives.append(one_movie)
+
+print(all_moives)
+
+for movie in all_moives:
+    one_str = movie[0] + " ," + movie[1] + "," + movie[2] + "," + movie[3] + "\n"
+    with open("movie.txt", "a", encoding="utf-8") as f:
+        f.write(one_str)
