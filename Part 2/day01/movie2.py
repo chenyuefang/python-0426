8075d509c35ee1010b11f7ced69af925cd67005a
Administrator <416326391@qq.com>
2019-05-10 16:54:11 +0800

    aletr

--- Part 2/day01/movie2.py
+++ Part 2/day01/movie2.py
@@ -1,5 +1,6 @@
 import requests
 from lxml import etree
+import re
 
 html = requests.get("https://movie.douban.com/top250")
 
@@ -11,12 +12,15 @@ movies_list = selector.xpath("//ol[@class='grid_view']/li")  # @属性名  @href
 for movie_selector in movies_list:
     name = movie_selector.xpath(".//div[@class='hd']/a/span[1]/text()")[0]  # 名称
     director = movie_selector.xpath(".//div[@class='bd']/p/text()")[0]  # 导演
+    director1 = re.findall("导演: (.*?) ", director)[0]  # "导演 (.*?) : 截取开头是导演+空格，到第一个空格之间的内容
+    act = re.findall("主演: (.*?) ", director)
+
     star = movie_selector.xpath(".//div[@class='star']/span[@class='rating_num']/text()")[0]  # 评分
     nums = movie_selector.xpath(".//div[@class='star']/span[last()]/text()")[0] # 人数
     nums = nums.split("人评价")[0]
     comment = movie_selector.xpath(".//div[@class='bd']/p[@class='quote']/span[@class='inq']/text()")[0]  # 评语
     url = movie_selector.xpath(".//div[@class='hd']/a/@href")[0]
-    one_movie = [name, director, star, nums, comment, url]
+    one_movie = [name, director1, star, nums, comment, url]
 
     all_moive.append(one_movie)
 
